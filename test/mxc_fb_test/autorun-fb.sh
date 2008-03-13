ca3d87e848da0663c197130d574897267afb636c
Rob Herring <r.herring@freescale.com>
2008-03-12 22:09:00 -0500

    ENGR00057358 Update fb and v4l2 tests for IPUv3
    Update fb and v4l2 tests for IPUv3
    
    Signed-off-by: Rob Herring <r.herring@freescale.com>

--- test/mxc_fb_test/autorun-fb.sh
+++ test/mxc_fb_test/autorun-fb.sh
@@ -13,7 +13,7 @@ check_devnode "/dev/fb0"
 export TERM=linux
 
 # Turn off blanking
-setterm -blank 0 > /dev/vc/0
+setterm -blank 0 > /dev/tty0
 
 
 # Blank test
@@ -26,9 +26,9 @@ echo -n 0 > /sys/class/graphics/fb0/blank
 # Color tests
 echo FB Color test
 if [ "$(platform)" = MXC27530EVB ]; then
-	bpp_list="16 24" 
+	bpp_list="16 24"
 else
-	bpp_list="16 24 32" 
+	bpp_list="16 24 32"
 fi
 for bpp in $bpp_list;
 do
@@ -39,29 +39,23 @@ do
 		echo FAIL - Unable to set bpp
 		STATUS=1
 	fi
-	setterm -inversescreen on > /dev/vc/0
-	setterm -foreground red > /dev/vc/0
-	setterm -clear all > /dev/vc/0
-	echo Screen is Red > /dev/vc/0
+	setterm -inversescreen on > /dev/tty0
+	setterm -foreground red > /dev/tty0
+	setterm -clear all > /dev/tty0
+	echo Screen is Red > /dev/tty0
 	sleep 1
-	setterm -foreground blue > /dev/vc/0
-	setterm -clear all > /dev/vc/0
-	echo Screen is Blue > /dev/vc/0
+	setterm -foreground blue > /dev/tty0
+	setterm -clear all > /dev/tty0
+	echo Screen is Blue > /dev/tty0
 	sleep 1
-	setterm -foreground green > /dev/vc/0
-	setterm -clear all > /dev/vc/0
-	echo Screen is Green > /dev/vc/0
+	setterm -foreground green > /dev/tty0
+	setterm -clear all > /dev/tty0
+	echo Screen is Green > /dev/tty0
 	sleep 1
 done
 
-# FB Suspend Test
-echo -n 3 > /sys/class/graphics/fb0/device/power/state
-echo Framebuffer is in state \(0 - run, 2 - suspend\):
-cat /sys/class/graphics/fb0/device/power/state
-echo -n 0 > /sys/class/graphics/fb0/device/power/state
-
 # Pan test
-# 
+#
 echo FB panning test
 echo 240,640 > /sys/class/graphics/fb0/virtual_size
 if ! grep -sq 240,640 /sys/class/graphics/fb0/virtual_size;
@@ -76,15 +70,15 @@ then
 fi
 
 for i in $(seq 1 50); do
-	echo This is line $i. > /dev/vc/0
+	echo This is line $i. > /dev/tty0
 done
 for i in $(seq 1 320); do
 	echo 0,$i > /sys/class/graphics/fb0/pan
-#	if ! grep -sq $i /sys/class/graphics/fb0/pan;
-#	then
-		# KERNEL BUG!!! echo FAIL - Unable to pan
-		# STATUS=1
-#	fi
+	if ! grep -sq $i /sys/class/graphics/fb0/pan;
+	then
+		echo FAIL - Unable to pan
+		STATUS=1
+	fi
 done
 
 print_status
