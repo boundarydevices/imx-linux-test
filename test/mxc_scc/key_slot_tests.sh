8ff866ea89707cfbaa80aff1f7b96ba8caf1dc1f
Yan Wang <r65094@freescale.com>
2008-07-25 10:57:41 +0800

    ENGR00058777 MX35: security drivers
    This CR is required to support security drivers in MX35 platform
    
    Signed-off-by: Yan Wang <r65094@freescale.com>

--- test/mxc_scc/key_slot_tests.sh
+++ test/mxc_scc/key_slot_tests.sh
@@ -15,7 +15,7 @@
 
 OWNER_ID=01234567fedcba98
 KEY_VALUE8=ffeeddcc00112233
-KEY_VALUE16=ffaaeebbdd44cc550066117722883399ffaaeebbdd44cc5500661177228833992
+KEY_VALUE33=ffaaeebbdd44cc550066117722883399ffaaeebbdd44cc550066117722883399ff
 SHORT_KEY=1234
 
 ############################################################################
@@ -26,7 +26,7 @@ SHORT_KEY=1234
 # Test strategy - Encrypt / Decrypt feature of SCC driver
 
 echo Allocate slot
-pos_test scc_test.out -Ka8:$OWNER_ID > /tmp/slot_$$
+pos_test scc_test -Ka8:$OWNER_ID > /tmp/slot_$$
 cat /tmp/slot_$$
 
 SLOT=`grep "^Slot.*is now al" /tmp/slot_$$`
@@ -34,25 +34,25 @@ SLOT=`echo $SLOT | sed -e 's/^Slot //' -e 's/ at .* is now allocated$//'`
 if [ -z $SLOT ]; then SLOT=-1; fi
 
 # Load slot with a key
-pos_test scc_test.out -Kl$SLOT:$OWNER_ID:r:$KEY_VALUE8
+pos_test scc_test -Kl$SLOT:$OWNER_ID:r:$KEY_VALUE8
 
 # Make invalid attempt to deallocate it (bad uid)
-neg_test scc_test.out -K$SLOT:1234
+neg_test scc_test -K$SLOT:1234
 
 # Try to load a too-large key into the slot
-neg_test scc_test.out -Kl$SLOT:$OWNER_ID:r:$KEY_VALUE16
+neg_test scc_test -Kl$SLOT:$OWNER_ID:r:$KEY_VALUE33
 
 # "Unload" a black version of our key
-pos_test scc_test.out -Ku$SLOT:$OWNER_ID > /tmp/key1_$$
+pos_test scc_test -Ku$SLOT:$OWNER_ID > /tmp/key1_$$
 cat /tmp/key1_$$
 BLACK1=`grep "^Encrypted val" /tmp/key1_$$ | sed -e 's/Encrypted value is //'`
 if [ -z $BLACK1 ]; then BLACK1=a; fi
 
 # Load a different key into the slot
-pos_test scc_test.out -Kl$SLOT:$OWNER_ID:r:$SHORT_KEY
+pos_test scc_test -Kl$SLOT:$OWNER_ID:r:$SHORT_KEY
 
 # Unload this value
-pos_test scc_test.out -Ku$SLOT:$OWNER_ID > /tmp/key2_$$
+pos_test scc_test -Ku$SLOT:$OWNER_ID > /tmp/key2_$$
 cat /tmp/key2_$$
 BLACK2=`grep "^Encrypted val" /tmp/key2_$$ | sed -e 's/Encrypted value is //'`
 if [ -z $BLACK2 ]; then BLACK2=b; fi
@@ -61,10 +61,10 @@ if [ -z $BLACK2 ]; then BLACK2=b; fi
 neg_test cmp /tmp/key1_$$ /tmp/key2_$$
 
 # Load original key in by its black version
-pos_test scc_test.out -Kl$SLOT:$OWNER_ID:b:$BLACK1
+pos_test scc_test -Kl$SLOT:$OWNER_ID:b:$BLACK1
 
 # Unload it
-pos_test scc_test.out -Ku$SLOT:$OWNER_ID > /tmp/key3_$$
+pos_test scc_test -Ku$SLOT:$OWNER_ID > /tmp/key3_$$
 BLACK3=`grep "^Encrypted val" /tmp/key3_$$ | sed -e 's/Encrypted value is //'`
 if [ -z $BLACK3 ]; then echo Broken > /tmp/key3_$$; fi
 
@@ -72,7 +72,7 @@ if [ -z $BLACK3 ]; then echo Broken > /tmp/key3_$$; fi
 pos_test cmp /tmp/key1_$$ /tmp/key3_$$
 
 # Deallocate slot
-pos_test scc_test.out -Kd$SLOT:$OWNER_ID
+pos_test scc_test -Kd$SLOT:$OWNER_ID
 
 # Clean up
 rm -f /tmp/key?_$$ /tmp/slot_$$
