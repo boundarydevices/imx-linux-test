5a9d5455f078cceed164c2fffc65bc9519740ee9
Wu Guoxing <b02248@freescale.com>
2008-07-29 16:47:18 +0800

    ENGR00085661 i2c slave unit test
    i2c slave unit test
    
    Signed-off-by: Guoxing Wu <b02248@freescale.com>

---
+++ test/mxc_i2c_slave_test/autorun-mxc_i2c_slave.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source /unit_tests/test-utils.sh
+
+#
+# Exit status is 0 for PASS, nonzero for FAIL
+#
+STATUS=0
+
+check_devnode "/dev/slave-i2c-0"
+run_testcase "./mxc_i2c_slave_test.out"
+
+print_status
+exit $STATUS
+
