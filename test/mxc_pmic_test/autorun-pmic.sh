83fb2521ed8b8267b6cb4d1d3814d15d4572c832
Jingyu Zhou <b02241@freescale.com>
2008-08-28 09:32:47 +0800

    ENGR00089467 Modify pmic unit test scripts and Makefile
    Modify pmic unit test scripts and Makefile
    
    Signed-off-by: Zhou Jingyu <Jingyu.Zhou@freescale.com>

--- test/mxc_pmic_test/autorun-pmic.sh
+++ test/mxc_pmic_test/autorun-pmic.sh
@@ -17,46 +17,34 @@ check_devnode "/dev/pmic_rtc"
 
 # protocol test cases
 for CASE in RW SU OC CA; do
-	run_testcase "./pmic_testapp -T $CASE"
+	run_testcase "./pmic_testapp.out -T $CASE"
 done
 
 # battery test cases
 echo "not running pmic_testapp_battery -T 6 as it hangs, TODO FIX"
 for CASE in 0 1 2 3 4 5; do
-	run_testcase "./pmic_testapp_battery -T $CASE"
+	run_testcase "./pmic_testapp_battery.out -T $CASE"
 done
 
 # RTC test cases
 for CASE in TIME ALARM WAIT_ALARM TEST POLL_TEST; do
-	run_testcase "./pmic_testapp_rtc -T $CASE"
+	run_testcase "./pmic_testapp_rtc.out -T $CASE"
 done
 
 # light test cases
 for CASE in 1 2 3 4 6 8; do
-	run_testcase "./pmic_testapp_light -T $CASE"
+	run_testcase "./pmic_testapp_light.out -T $CASE"
 done
 for BANK in 1 2 3; do
 	for COLOR in 1 2 3; do
-		run_testcase "./pmic_testapp_light -T 7 -B $BANK -C $COLOR"
-	done		
+		run_testcase "./pmic_testapp_light.out -T 7 -B $BANK -C $COLOR"
+	done
 done
 for BANK in 1 2 3; do
 	for PATTERN in 1 2 3 4 5 6 7 8 9 10 11 12; do
-		run_testcase "./pmic_testapp_light -T 5 -B $BANK -F $PATTERN"
-	done		
-done
-
-# power test cases
-# insmod test
-insmod_test /lib/modules/$(kernel_version)/test/mxc_pmic_power_testmod.ko
-# devnode test
-check_devnode "/dev/pmic_power"
-
-if [ "$STATUS" = 0 ]; then
-	for CASE in 1 2 3; do
-		run_testcase "./pmic_testapp_power -T $CASE"
+		run_testcase "./pmic_testapp_light.out -T 5 -B $BANK -F $PATTERN"
 	done
-fi
+done
 
 print_status
 exit $STATUS
