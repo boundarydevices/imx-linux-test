27db74c4b60875f5ba7635b930c3c20a73dfdfbd
Liu Ying <b17645@freescale.com>
2009-11-10 09:35:45 -0500

    ENGR00118023 IPU lib unit test:Play horizontally splitted video on TV
    Support to play horizontally splitted video on TV.
    This should be able to verify the feasibility of using IPU library to
    play 720P video on TV(including IC channel upsizing).
    This new test assumes the TV uses MEM_DC_SYNC channel.
    
    Signed-off-by: Liu Ying <b17645@freescale.com>

--- test/mxc_ipudev_test/mxc_ipudev_test.c
+++ test/mxc_ipudev_test/mxc_ipudev_test.c
@@ -132,13 +132,14 @@ int main(int argc, char *argv[])
 			"12: 3 screen layer test using IC local alpha blending with alpha value in separate buffer\n" \
 			"13: 2 screen layer test using IC local alpha blending with alpha value in pixel\n" \
 			"14: 3 screen layer test using IC local alpha blending with alpha value in pixel\n" \
-			"15: 2 screen layer test IPC ProcessA + ProcessB with globla alpha blending)\n" \
-			"16: 2 screen layer test IPC ProcessA + ProcessB with local alpha blending)\n" \
-			"17: 3 screen layer test IPC ProcessA(first_layer + sencond_layer) + ProcessB(third_layer) with globla alpha blending)\n" \
-			"18: 3 screen layer test IPC ProcessA(first_layer + sencond_layer) + ProcessB(third_layer) with local alpha blending)\n" \
-			"19: 3 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) ProcessC(third_layer) with local alpha blending)\n" \
-			"20: 2 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) with DP local alpha blending)\n" \
-			"21: 2 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) with local alpha blending plus tv copy)\n\n");
+			"15: 2 screen layer test IPC ProcessA + ProcessB with globla alpha blending\n" \
+			"16: 2 screen layer test IPC ProcessA + ProcessB with local alpha blending\n" \
+			"17: 3 screen layer test IPC ProcessA(first_layer + sencond_layer) + ProcessB(third_layer) with globla alpha blending\n" \
+			"18: 3 screen layer test IPC ProcessA(first_layer + sencond_layer) + ProcessB(third_layer) with local alpha blending\n" \
+			"19: 3 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) ProcessC(third_layer) with local alpha blending\n" \
+			"20: 2 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) with DP local alpha blending\n" \
+			"21: 2 screen layer test IPC ProcessA(first_layer) ProcessB(sencond_layer) with local alpha blending plus tv copy\n" \
+			"22: Horizontally splitted video test on TV(support upsizing), assuming the TV uses MEM_DC_SYNC channel\n\n");
 		return -1;
 	}
 
