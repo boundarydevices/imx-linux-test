7b9c71a4b5ce1dfd0e4934f2b2cd73d76687abbd
Liu Ying <b17645@freescale.com>
2010-01-08 15:44:13 -0500

    ENGR00119710 MXC V4L2 output:Support YUYV input pixel format
    Support YUYV input pixel format for mxc V4L2 output unit test.
    
    Signed-off-by: Liu Ying <b17645@freescale.com>

--- test/mxc_v4l2_test/mxc_v4l2_output.c
+++ test/mxc_v4l2_test/mxc_v4l2_output.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2009 Freescale Semiconductor, Inc. All rights reserved.
+ * Copyright 2004-2010 Freescale Semiconductor, Inc. All rights reserved.
  */
 
 /*
@@ -494,6 +494,7 @@ int process_cmdline(int argc, char **argv)
                              (g_in_fmt != V4L2_PIX_FMT_RGB565) &&
                              (g_in_fmt != 'PMBW') &&
                              (g_in_fmt != V4L2_PIX_FMT_UYVY) &&
+			     (g_in_fmt != V4L2_PIX_FMT_YUYV) &&
 			     (g_in_fmt != V4L2_PIX_FMT_YUV422P) &&
                              (g_in_fmt != V4L2_PIX_FMT_YUV420) &&
                              (g_in_fmt != V4L2_PIX_FMT_NV12) )
