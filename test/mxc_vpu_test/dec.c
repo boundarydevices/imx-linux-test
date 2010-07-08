7266799f29259dae36bef6e816c860415815399e
Sammy He <r62914@freescale.com>
2010-07-08 10:21:11 +0800

    ENGR00124196 vpu: Change vpu unit test to sync with v1.4.0 firmware
    Change vpu test to sync with v1.4.0 firmware on mx51 and mx53;
    Remove underrun processing since the feature is dropped from
    firmware.
    
    Signed-off-by: Sammy He <r62914@freescale.com>

--- test/mxc_vpu_test/dec.c
+++ test/mxc_vpu_test/dec.c
@@ -807,12 +807,6 @@ decoder_start(struct decode *dec)
 			vpu_DecGiveCommand(handle, SET_DBK_OFFSET, &dbkOffset);
 		}
 
-		/* RTC setting */
-		decparam.underrunEnable = 0;
-		decparam.accumulativeMode = 0;
-		decparam.pulseWidth = 0;
-		decparam.refMbClk = 0;
-
 		gettimeofday(&tdec_begin, NULL);
 		ret = vpu_DecStartOneFrame(handle, &decparam);
 		if (ret != RETCODE_SUCCESS) {
