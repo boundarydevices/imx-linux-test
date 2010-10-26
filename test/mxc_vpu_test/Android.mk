0ece9a5e987ff812b48e97a617c9bae76d15e00a
Sammy He <r62914@freescale.com>
2010-10-26 23:28:19 +0800

    ENGR00132963 Add vpu unit test support on Android
    Make vpu unit test can work on android.
    
    Signed-off-by: Sammy He <r62914@freescale.com>

---
+++ test/mxc_vpu_test/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+       dec.c \
+       capture.c \
+       display.c \
+       enc.c \
+       fb.c \
+       loopback.c \
+       utils.c \
+       main.c
+
+LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH) \
+	external/linux-lib/ipu \
+	external/linux-lib/vpu
+
+LOCAL_SHARED_LIBRARIES := libutils libc libvpu libipu
+
+LOCAL_MODULE := mxc-vpu-test
+LOCAL_MODULE_TAGS := tests
+include $(BUILD_EXECUTABLE)
