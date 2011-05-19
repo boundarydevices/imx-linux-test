ef474937e5d5259cc4961ab871ac5e70a6aac065
Zhang Jiejing <jiejing.zhang@freescale.com>
2011-05-19 14:12:41 +0800

    ENGR00140702 bluetooth: update bluetooth init binary
    Update bluetooth init binary and source code to align
    kernel's driver update.
    
    it fix the file transfer bug.
    and use 3M bps bandrate.
    
    Signed-off-by: Zhang Jiejing <jiejing.zhang@freescale.com>
    Acked-by: Lily Zhang <r58066@freescale.com>

--- test/ar3k_bt/hciattach.h
+++ test/ar3k_bt/hciattach.h
@@ -33,20 +33,27 @@
 #define HCIUARTSETPROTO		_IOW('U', 200, int)
 #define HCIUARTGETPROTO		_IOR('U', 201, int)
 #define HCIUARTGETDEVICE	_IOR('U', 202, int)
+#define HCIUARTSETFLAGS		_IOW('U', 203, int)
+#define HCIUARTGETFLAGS		_IOR('U', 204, int)
 
 #define HCI_UART_H4	0
 #define HCI_UART_BCSP	1
 #define HCI_UART_3WIRE	2
 #define HCI_UART_H4DS	3
 #define HCI_UART_LL	4
-#define HCI_UART_ATH    5
+#define HCI_UART_ATH3K  5
+
+#define HCI_UART_RAW_DEVICE	0
 
 int read_hci_event(int fd, unsigned char* buf, int size);
 int set_speed(int fd, struct termios *ti, int speed);
 
-//int texas_init(int fd, struct termios *ti);
-//int texas_post(int fd, struct termios *ti);
-//int texasalt_init(int fd, int speed, struct termios *ti);
-//int stlc2500_init(int fd, bdaddr_t *bdaddr);
-//int bgb2xx_init(int dd, bdaddr_t *bdaddr);
-int ath_ps_download(int fd);
+int texas_init(int fd, struct termios *ti);
+int texas_post(int fd, struct termios *ti);
+int texasalt_init(int fd, int speed, struct termios *ti);
+int stlc2500_init(int fd, bdaddr_t *bdaddr);
+int bgb2xx_init(int dd, bdaddr_t *bdaddr);
+int ath3k_init(int fd, int speed, int init_speed, char *bdaddr,
+						struct termios *ti);
+int ath3k_post(int fd, int pm);
+int qualcomm_init(int fd, int speed, struct termios *ti, const char *bdaddr);
