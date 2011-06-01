9d47b3ee26aa7ec429d4535e45301343787732cd
Lily Zhang <r58066@freescale.com>
2011-06-01 09:31:04 +0800

    ENGR00143958 ar3k_bt: change file path as absolute path
    change hciattach-ar3k.bin file path in the script file
    as absolute path.
    
    Signed-off-by: Lily Zhang <r58066@freescale.com>

--- test/ar3k_bt/ar3k-bt-start.sh
+++ test/ar3k_bt/ar3k-bt-start.sh
@@ -3,4 +3,4 @@
 sudo pkill -9 hciattach-ar3k.bin
 sudo su -c "echo 1 > /sys/class/rfkill/rfkill0/state"
 sudo chmod 777 /dev/ttymxc2
-sudo ./hciattach-ar3k.bin -n ttymxc2 ath3k 3000000 flow sleep &
+sudo /unit_tests/hciattach-ar3k.bin -n ttymxc2 ath3k 3000000 flow sleep &
