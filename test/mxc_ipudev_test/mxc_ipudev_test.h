70fca4768015224904b080ba5e87cd21fad43873
Jason Chen <b02280@freescale.com>
2011-08-22 10:54:20 +0800

    ENGR00155135 ipudev test: test for processing driver
    IPU's IC/IRT/VDI modules provide resizing/CSC/combination/de-interlacing
    support, this patch make all these features into one processing driver.
    A struct ipu_task is the interface between user and this driver, user just
    need fill his task struct and queue it through ioctl, then wait ipu hardware
    finish its job (now only support BLOCKING operation, not support NO_BLOCK
    operation).
    Pls refer to inlcude/linux/ipu.h for structure information and unit test
    for usage.
    
    This patch is for unit test changes.
    
    Signed-off-by: Jason Chen <b02280@freescale.com>

--- test/mxc_ipudev_test/mxc_ipudev_test.h
+++ test/mxc_ipudev_test/mxc_ipudev_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2009-2011 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  */
 
@@ -23,35 +23,15 @@
 #define __MXC_IPUDEV_TEST_H__
 
 #include <linux/ipu.h>
-#include <linux/videodev.h>
-#include "mxc_ipu_hl_lib.h"
 
 typedef struct {
-	ipu_lib_handle_t * ipu_handle;
+	struct ipu_task task;
 	int fcount;
 	int loop_cnt;
-	int mode;
-	int test_pattern;
-	int block_width;
-	int query_task;
-	int kill_task;
-	int kill_task_idx;
+	int show_to_fb;
 	char outfile[128];
-	FILE * file_out;
-	ipu_lib_input_param_t input;
-	ipu_lib_output_param_t output;
 } ipu_test_handle_t;
 
-enum {
-	NO_OV = 0x00,
-	IC_GLB_ALP_OV = 0x01,
-	IC_LOC_SEP_ALP_OV = 0x02,
-	IC_LOC_PIX_ALP_OV = 0x04,
-	DP_LOC_SEP_ALP_OV = 0x08,
-	COPY_TV = 0x10,
-};
-
 extern int parse_config_file(char *file_name, ipu_test_handle_t *test_handle);
-int run_test_pattern(int pattern, ipu_test_handle_t * test_handle);
 
 #endif
