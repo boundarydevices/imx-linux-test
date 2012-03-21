b0f869fcdff6da5a4326c9c7e0f273f7f6241ee5
Lily Zhang <r58066@freescale.com>
2012-03-21 14:29:19 +0800

    ENGR00177387 Fix build error of missing videodev.h
    videodev.h is removed from latest kernel header file.
    Replace videodev.h as videodev2.h to fix build error.
    
    Signed-off-by: Lily Zhang <r58066@freescale.com>

--- test/mxc_v4l2_test/mxc_v4l2_capture.c
+++ test/mxc_v4l2_test/mxc_v4l2_capture.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2011 Freescale Semiconductor, Inc. All rights reserved.
+ * Copyright 2004-2012 Freescale Semiconductor, Inc. All rights reserved.
  */
 
 /*
@@ -38,7 +38,7 @@ extern "C"{
 #include <stdio.h>
 #include <stdlib.h>
 #include <asm/types.h>
-#include <linux/videodev.h>
+#include <linux/videodev2.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <malloc.h>
