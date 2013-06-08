34627b38a41eceb8b53329faab2b9a11f2bbbe0e
Nicolin Chen <B42378@freescale.com>
2013-06-08 17:17:33 +0800

    ENGR00266287 mxc: tuner: search si4763 in script
    Previously we used a fixed number, that's no good, so add search code.
    
    Signed-off-by: Nicolin Chen <B42378@freescale.com>

--- test/mxc_tuner_test/mxc_tuner_test.sh
+++ test/mxc_tuner_test/mxc_tuner_test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-arecord -D hw:2,0 -f dat | aplay -D hw:0,0 -f dat &
+num=`arecord -l |grep -i "si4763" |awk '{ print $2 }'|sed 's/://'`
+
+arecord -D hw:$num,0 -f dat | aplay -D hw:0,0 -f dat &
 ./mxc_tuner_test.out
-pid=$(ps | grep "arecord -D hw:2,0 -f dat" | grep -v ' grep ' | awk '{print $1}')
+pid=$(ps | grep "arecord -D hw:$num,0 -f dat" | grep -v ' grep ' | awk '{print $1}')
 echo "killing pid = $pid"
 kill $pid
 
