d38f85d7458630256002623c624dba2316255203
Nicolin Chen <b42378@freescale.com>
2013-08-29 16:35:46 +0800

    ENGR00276567-2 mxc: tuner: Add si476x driver support
    The frequence setting for si476x and old si4763 drivers are a bit different.
    Thus add checking code to detect which driver is being used.
    
    Acked-by: Wang Shengjiu <b02247@freescale.com>
    Signed-off-by: Nicolin Chen <b42378@freescale.com>

--- test/mxc_tuner_test/mxc_tuner_test.sh
+++ test/mxc_tuner_test/mxc_tuner_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-num=`arecord -l |grep -i "si4763" |awk '{ print $2 }'|sed 's/://'`
+num=`arecord -l |grep -i "si476" |awk '{ print $2 }'|sed 's/://'`
 
 arecord -D hw:$num,0 -f dat | aplay -D hw:0,0 -f dat &
 ./mxc_tuner_test.out
