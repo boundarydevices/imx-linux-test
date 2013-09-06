1bec146782daa55f9c9a462dc17e75b0aebed717
Jason Liu <r64343@freescale.com>
2013-09-06 12:37:02 +0800

    ENGR00278625-1 fix the dump-clocks issues due to sys interface change
    the sys for the clk interface has been changed, this patch update it
    to reflect the new interface.
    
    Signed-off-by: Jason Liu <r64343@freescale.com>

--- test/mxc_clock_test/dump-clocks.sh
+++ test/mxc_clock_test/dump-clocks.sh
@@ -6,27 +6,40 @@ fi
 
 saved_path=$PWD
 
-printf "%-24s %-24s %3s %9s\n" "clock" "parent" "use" "rate"
+printf "%-24s %-24s %8s %6s %6s %9s\n" "clock" "parent" "flags" "en_cnt" "pre_cnt" "rate"
 
-for foo in $(find /sys/kernel/debug/clock -type d); do
-    if [ "$foo" = '/sys/kernel/debug/clock' ]; then
+for foo in $(find /sys/kernel/debug/clk -type d); do
+    if [ "$foo" = '/sys/kernel/debug/clk' ]; then
         continue
     fi
 
     cd $foo
 
-    use="$(cat usecount)"
-    rate="$(cat rate)"
+    if [ -f clk_flags ]; then
+        flags="$(cat clk_flags)"
+    fi
+
+    if [ -f clk_enable_count ]; then
+        enable_count="$(cat clk_enable_count)"
+    fi
+
+    if [ -f clk_prepare_count ]; then
+        prepare_count="$(cat clk_enable_count)"
+    fi
+
+    if [ -f clk_rate ]; then
+        rate="$(cat clk_rate)"
+    fi
 
     clk="$(basename $foo)"
     cd ..
     parent="$(basename $PWD)"
 
-    if [ "$parent" = 'clock' ]; then
+    if [ "$parent" = 'clk' ]; then
         parent="   ---"
     fi
 
-    printf "%-24s %-24s %2d %10d\n" "$clk" "$parent" "$use" "$rate"
+    printf "%-24s %-24s %08x %6d %6d %10d\n" "$clk" "$parent" "$flags" "$enable_count" "$prepare_count" "$rate"
 
     cd $saved_path
 done
