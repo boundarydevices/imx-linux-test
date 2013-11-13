565ea932b847a0475ab520186e619b6a7666fa42
Nicolin Chen <b42378@freescale.com>
2013-11-13 12:05:27 +0800

    ENGR00286961 unit-test: mxc_tuner_test: use better way to get pid
    ps command might be different in different rootfs, it sometimes would fail to
    be executed. So it's not an ideal way to get pid by using it. Thus change it.
    
    Tested with 3.0.35 LTIB rootfs and newest Yocto rootfs.
    
    Signed-off-by: Nicolin Chen <b42378@freescale.com>

--- test/mxc_tuner_test/mxc_tuner_test.sh
+++ test/mxc_tuner_test/mxc_tuner_test.sh
@@ -3,8 +3,8 @@
 num=`arecord -l |grep -i "si476" |awk '{ print $2 }'|sed 's/://'`
 
 arecord -D hw:$num,0 -f dat | aplay -D hw:0,0 -f dat &
+pid=$!
 ./mxc_tuner_test.out
-pid=$(ps | grep "arecord -D hw:$num,0 -f dat" | grep -v ' grep ' | awk '{print $1}')
 echo "killing pid = $pid"
 kill $pid
 
