bd69f0398b05d0aa3a418cd4fa7ce9e13e545f6d
Hongzhang Yang <Hongzhang.Yang@freescale.com>
2013-12-10 16:08:07 +0800

    ENGR00292579-1 Integrate G2D renderer for Android
    Integrate Xianzhong's implementation of G2D renderer (based on JB4.2.2).
    
    Signed-off-by: Xianzhong <b07117@freescale.com>
    Signed-off-by: Hongzhang Yang <Hongzhang.Yang@freescale.com>

--- test/mxc_vpu_test/Android.mk
+++ test/mxc_vpu_test/Android.mk
@@ -10,6 +10,7 @@ LOCAL_SRC_FILES := \
        fb.c \
        loopback.c \
        transcode.c \
+       android_display.cpp \
        utils.c \
        main.c
 
@@ -17,9 +18,10 @@ LOCAL_CFLAGS += -DBUILD_FOR_ANDROID
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 	external/linux-lib/ipu \
-	external/linux-lib/vpu
+	external/linux-lib/vpu \
+	device/fsl-proprietary/include
 
-LOCAL_SHARED_LIBRARIES := libutils libc libvpu libipu
+LOCAL_SHARED_LIBRARIES := libutils libc libvpu libipu libg2d libgui libui libhardware
 
 LOCAL_MODULE := mxc-vpu-test
 LOCAL_MODULE_TAGS := tests
