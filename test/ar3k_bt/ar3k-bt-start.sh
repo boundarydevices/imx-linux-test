312bc9552ed07ad0becaf87674d786e73019b57e
Fugang Duan <b38611@freescale.com>
2015-10-12 11:14:44 +0800

    MLK-11688 ar3k: remove the ar3k test tool
    Remove the ar3k hciattach test tools since bluez4.x and bluez5.x all
    support ar3k/st/broadcom BT and others more BT vendors.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>

--- test/ar3k_bt/ar3k-bt-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo pkill -9 hciattach-ar3k.bin
-sudo su -c "echo 1 > /sys/class/rfkill/rfkill0/state"
-sudo chmod 777 /dev/ttymxc2
-sudo /unit_tests/hciattach-ar3k.bin -n ttymxc2 ath3k 3000000 flow sleep &
